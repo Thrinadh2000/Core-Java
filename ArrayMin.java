60f72e6e7f68a5d58604eae97cfa64142c4bf96f
Peddi Thrinadh <thrinadhpeddi2000@gmail.com>
2025-09-26 15:48:04 -0400

    new branch local to remote repositary

--- ArrayMin.java
+++ ArrayMin.java
@@ -1,1 +1,20 @@
- 
+public class ArrayMin {
+
+    public static void main(String[] args) {
+
+
+        int[] a = {45,76,23,98,34};
+        int min = a[0];
+        for(int i=0;i <a.length;i++)
+        {
+            if(a[i] < min)
+                min = a[i];
+
+
+        }
+
+        System.out.println(min);
+    }
+
+}
+
